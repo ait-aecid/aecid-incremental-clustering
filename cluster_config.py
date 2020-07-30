fb98828f67eff0fca6b8a888ae52508a8c1e0a4f
landauerm <max.landauer@ait.ac.at>
2020-07-30 14:44:49 +0200

    Change threshold to 0.51

--- cluster_config.py
+++ cluster_config.py
@@ -1,4 +1,4 @@
-st = 0.52 # Similarity threshold for clustering [0, 1]
+st = 0.51 # Similarity threshold for clustering [0, 1]
 input_file = 'data/in/mainlog' # Path to input log file
 output_file = 'data/out/clusters.txt' # Path to output file
 timestamp_length = 19 # Length of time stamp at beginning of log line that will be removed; set to -1 for no timestamp
